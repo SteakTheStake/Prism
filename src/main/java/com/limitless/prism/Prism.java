00327444a20cc58162c0e764a21aeda6b906c39b
SteakTheStake <gabecorr25@gmail.com>
2023-09-14 23:44:20 -0700

    fixed biome colors

--- src/main/java/com/limitless/prism/Prism.java
+++ src/main/java/com/limitless/prism/Prism.java
@@ -8,6 +8,9 @@ import com.limitless.prism.util.ModRegistries;
 import com.limitless.prism.world.gen.ModWorldGeneration;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.GrassColors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,5 +27,12 @@ public class Prism implements ModInitializer {
 		ModItems.registerModItems();
 		ModWorldGeneration.generateModWorldGeneration();
 		ModScreenHandlers.registerScreenHandler();
+
+		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0x82BD5F, ModBlocks.GRASS_TERRAIN);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 0x82BD5F, ModBlocks.GRASS_TERRAIN);
+		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0x82BD5F, ModBlocks.GARDEN_CROTON);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 0x82BD5F, ModBlocks.GARDEN_CROTON);
+		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0x82BD5F, ModBlocks.TALL_GARDEN_CROTON);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 0x82BD5F, ModBlocks.TALL_GARDEN_CROTON);
 	}
 }
